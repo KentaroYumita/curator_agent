afb8ea527bbbff719ca198a794ac647c380625f4
Makoto-Takeuchi <t419mako@gmail.com>
2021-07-04 21:50:39 +0900

    7/4 21:50

--- src/main/webapp/src/main.js
+++ src/main/webapp/src/main.js
@@ -1,11 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 //import VueCropper from 'cropperjs';
 import 'cropperjs/dist/cropper.css';
 
-import axios from 'axios' //追記
-import VueAxios from 'vue-axios' //追記
-Vue.use(VueAxios, axios) //追記
 
 createApp(App).use(router).mount('#app')
