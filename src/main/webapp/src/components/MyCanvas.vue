c3d872f19acecbd27a5c44a61ee39eda4ac98e2a
KentaroYumita <m5251202@u-aizu.ac.jp>
2021-09-19 11:25:09 +0900

    グラフの描画、センサーデータの取得

--- src/main/webapp/src/components/MyCanvas.vue
+++ src/main/webapp/src/components/MyCanvas.vue
@@ -10,18 +10,20 @@ export default {
       canvas: null,
       ctx: null,
 
-      points: [
-          {x:950, y:270},
-        {x:720, y:270},
-        {x:470, y:270},
-        {x:470, y:480},
-        {x:300, y:480},
-        {x:200, y:480},
-        {x:60, y:480},
-        {x:200, y:270},
-        {x:200, y:220},
-        {x:700, y:100},
-        {x:750, y:100},
+      // センサー情報
+      // シリアル番号は一致していない
+      sensor: [
+          {x:850, y:250, serial:"dca63282b7a3", people:[]},
+        {x:680, y:250, serial:"dca63282b8c9", people:[]},
+        {x:450, y:250, serial:"dca63282b8d2", people:[]},
+        {x:450, y:450, serial:"dca63282b98f", people:[]},
+        {x:300, y:450, serial:"dca63282b9b6", people:[]},
+        {x:170, y:450, serial:"dca63282b9da", people:[]},
+        {x:30, y:450, serial:"dca63282b9e3", people:[]},
+        {x:150, y:250, serial:"dca63282ba53", people:[]},
+        {x:150, y:190, serial:"dca63282ba85", people:[]},
+        {x:610, y:100, serial:"dca63282ba98", people:[]},
+        {x:740, y:100, serial:"dca63282baaf", people:[]},
       ],
     }
   },
@@ -31,6 +33,7 @@ export default {
     }
   },
   methods: {
+    // 地図の表示
     draw() {
       this.ctx.clearRect(0, 0, 200, 200)
 
@@ -39,29 +42,68 @@ export default {
       img.src = AssetImage
       img.onload = () => {
         this.ctx.drawImage(img, 0, 0)
+      }
+    },
 
-        // センサー毎に描画
-        console.log(this.points)
-        this.ctx.strokeStyle = "#000000"
-        this.ctx.font = "20px normal"
-        for(let i=0; i<this.points.length; i++) {
-          let p = this.points[i]
+    // センサー毎に描画
+    drawPeople(sensor){
+      this.ctx.font = "20px normal"
 
-          this.ctx.beginPath()
-          this.ctx.arc(p.x, p.y, 20, 0, Math.PI * 2)
-          this.ctx.fillStyle = "#FFFF00"
-          this.ctx.fill()
-          this.ctx.stroke()
-          this.ctx.fillStyle = "#000000"
-          this.ctx.fillText(i+1,p.x-5, p.y+2)
+      for(let i=0; i<this.sensor.length; i++) {
+        // グラフの背景
+        this.ctx.fillStyle = "#FFFFFF"
+        this.ctx.fillRect(sensor.x,sensor.y,110,50)
+
+        // 各データの描画
+        let people_data = sensor.people
+        for(let j=0; j<people_data.length; j++) {
+          let people = sensor.people[j];
+
+          // 少ないと青、中程度だと紫、多いと赤
+          this.ctx.fillStyle = people <= 3 ? "#3333FF" : (people <= 7 ? "#FF33FF" : "#FF3333")
+          this.ctx.fillRect(sensor.x + 10 * j, sensor.y + 5 * (10 - people), 10, 5 * people)
         }
+
+        // 枠線
+        this.ctx.strokeStyle = "#000000"
+        this.ctx.strokeRect(sensor.x,sensor.y,110,50)
+
+        // グラフの中央を表すマーク
+        this.ctx.fillStyle = "#FFFF00"
+        this.ctx.strokeStyle = "#000000"
+        this.ctx.beginPath()
+        this.ctx.arc(sensor.x+10*5+5, sensor.y+5*10,5, 0 * Math.PI / 180, 360 * Math.PI / 180, false );
+        this.ctx.fill()
+        this.ctx.stroke()
+      }
+    },
+
+    // センサーからデータ取得
+    getSensor() {
+      for (let i = 0; i < this.sensor.length; i++) {
+        fetch("https://teamapps.u-aizu.ac.jp/sense/presence/"+this.sensor[i].serial)
+            .then(response => {
+              return response.json()
+            })
+            .then(data => {
+              // 11個のデータを取得
+              for (let j = 0; j < 11; j++) {
+                this.sensor[i].people.push(data[j].reading)
+                this.drawPeople(this.sensor[i])
+              }
+              console.log(data)
+            }).catch(error => {
+          console.log('error: ' + error)
+        })
       }
     }
   },
   mounted() {
     this.canvas = this.$refs.canvas
     this.ctx = this.canvas.getContext('2d')
+
     this.draw()
+    this.getSensor()
   },
 }
 </script>
--- src/main/webapp/src/router/index.ts
+++ src/main/webapp/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import exhibitionList from "../views/exhibitionList.vue";
 
 const routes = [
@@ -31,7 +31,7 @@ const routes = [
 
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes
 })
 
