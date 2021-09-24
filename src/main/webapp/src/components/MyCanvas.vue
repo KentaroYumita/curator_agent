b0a1fe4d7e8362fa16fa3ba4f4b71c21ecff568b
KentaroYumita <m5251202@u-aizu.ac.jp>
2021-09-24 11:33:59 +0900

    人数の増減による、矢印を引くかどうかの判定を作成

--- src/main/webapp/src/components/MyCanvas.vue
+++ src/main/webapp/src/components/MyCanvas.vue
@@ -1,11 +1,14 @@
 <template>
-  <canvas ref="canvas" width="1234" height="623" class="canvas"></canvas>
+  <div id="my_canvas">
+    <canvas ref="canvas" width="1234" height="623" class="canvas"></canvas>
+  </div>
 </template>
 
 <script>
 import AssetImage from "@/assets/fks_museum_layout_c.png"
 
 export default {
+  el: '#my_canvas',
   data(){
     return {
       canvas: null,
@@ -51,7 +54,7 @@ export default {
   methods: {
     // 地図の表示
     draw() {
-      this.ctx.clearRect(0, 0, 200, 200)
+      this.ctx.clearRect(0, 0, 1234, 623)
 
       // 地図を背景にする
       let img = new Image()
@@ -94,54 +97,108 @@ export default {
       }
     },
 
+    // 全ての矢印の描画
+    drawAllArrows(){
+      for(let i=0; i<this.route.length; i++){
+        for(let j=0; j<this.route[i].length; j++){
+          // 始点
+          let start = {x: this.sensor[i].x + 110/2, y: this.sensor[i].y + 50/2, people: this.sensor[i].people}
+          // 終点
+          let end = {x: this.sensor[this.route[i][j]].x + 110/2, y: this.sensor[this.route[i][j]].y + 50/2, people: this.sensor[this.route[i][j]].people}
+
+          // 矢印を引けるか判定
+          if(this.checkArrow(start.people, end.people)) {
+            this.drawArrow(start, end)
+            break
+          }
+        }
+      }
+    },
+
+    // 矢印が引けるかどうか判定
+    checkArrow(startP, endP){
+      // 始点における人数の増減
+      let startFluc = []
+      for(let i=0; i<startP.length-1; i++){
+        startFluc.push(startP[i+1]-startP[i])
+      }
+
+      // 終点における人数の増減
+      let endFluc = []
+      for(let i=0; i<endP.length-1; i++){
+        endFluc.push(endP[i+1]-endP[i])
+      }
+
+      for(let i=0; i<startFluc.length; i++){
+
+        // 始点で減った場所について調べる
+        if(startFluc[i] >= 0) continue;
+        for(let j=i; j<endFluc.length; j++){
+
+          // 始点で減った後で、終点が増えた地点がある場合
+          // 終点に向けて矢印を引く
+          if(endFluc[j] + startFluc[i] >= 0){
+            return true
+          }
+        }
+      }
+      return false
+    },
+
     // 矢印の描画
-    drawArrow(){
+    drawArrow(start, end) {
       this.ctx.lineWidth = "5"
       this.ctx.fillStyle = "#FFFF00"
       this.ctx.strokeStyle = "#FF0000"
 
-      for(let i=0; i<this.route.length; i++){
-        for(let j=0; j<this.route[i].length; j++){
-          // 始点
-          let start = {x: this.sensor[i].x + 110/2, y: this.sensor[i].y + 50}
-          // 終点
-          let end = {x: this.sensor[this.route[i][j]].x + 110/2, y: this.sensor[this.route[i][j]].y + 50}
+      // 矢印の長さ
+      let len = Math.sqrt((start.x - end.x) * (start.x - end.x) + (start.y - end.y) * (start.y - end.y))
+      // 先端の大きさ
+      let arrowSize = 20
 
-          // 矢印の長さ
-          let len = Math.sqrt((start.x-end.x)*(start.x-end.x) + (start.y-end.y)*(start.y-end.y))
-          // 先端の大きさ
-          let arrowSize = 20
-
-          // 線分 start-end 上の点、矢印の境目
-          // 線分 start-end を (len-arrowSize : len) で内分する点
-          let p = {x:(start.x*arrowSize+(len-arrowSize)*end.x)/len, y:(start.y*arrowSize+(len-arrowSize)*end.y)/len}
-
-          // 矢印の先端を成す点1
-          let sin = Math.sin(Math.PI/4)
-          let cos = Math.cos(Math.PI/4)
-          let p1 = {x:(p.x-end.x)*cos-(p.y-end.y)*sin+end.x, y:(p.x-end.x)*sin+(p.y-end.y)*cos+end.y}
-
-          // 矢印の先端を成す点2
-          sin = Math.sin(-Math.PI/4)
-          cos = Math.cos(-Math.PI/4)
-          let p2 = {x:(p.x-end.x)*cos-(p.y-end.y)*sin+end.x, y:(p.x-end.x)*sin+(p.y-end.y)*cos+end.y}
-
-          // 線分の描画
-          this.ctx.beginPath()
-          this.ctx.moveTo(start.x, start.y)
-          this.ctx.lineTo(end.x, end.y)
-          this.ctx.stroke()
-
-          // 先端の描画
-          this.ctx.beginPath()
-          this.ctx.moveTo(end.x,end.y)
-          this.ctx.lineTo(p1.x,p1.y)
-          this.ctx.lineTo(p2.x,p2.y)
-          this.ctx.lineTo(end.x,end.y)
-          this.ctx.fill()
-          this.ctx.stroke()
-        }
+      // 線分 start-end 上の点、矢印の境目
+      // 線分 start-end を (len-arrowSize : len) で内分する点
+      let p = {
+        x: (start.x * arrowSize + (len - arrowSize) * end.x) / len,
+        y: (start.y * arrowSize + (len - arrowSize) * end.y) / len
       }
+
+      // 矢印の先端を成す点1
+      let sin = Math.sin(Math.PI / 4)
+      let cos = Math.cos(Math.PI / 4)
+      let p1 = {
+        x: (p.x - end.x) * cos - (p.y - end.y) * sin + end.x,
+        y: (p.x - end.x) * sin + (p.y - end.y) * cos + end.y
+      }
+
+      // 矢印の先端を成す点2
+      sin = Math.sin(-Math.PI / 4)
+      cos = Math.cos(-Math.PI / 4)
+      let p2 = {
+        x: (p.x - end.x) * cos - (p.y - end.y) * sin + end.x,
+        y: (p.x - end.x) * sin + (p.y - end.y) * cos + end.y
+      }
+
+      // 始点の描画
+      this.ctx.beginPath()
+      this.ctx.arc(start.x,start.y,5,0,Math.PI*2)
+      this.ctx.fill()
+      this.ctx.stroke()
+
+      // 線分の描画
+      this.ctx.beginPath()
+      this.ctx.moveTo(start.x, start.y)
+      this.ctx.lineTo(end.x, end.y)
+      this.ctx.stroke()
+
+      // 先端の描画
+      this.ctx.beginPath()
+      this.ctx.moveTo(end.x, end.y)
+      this.ctx.lineTo(p1.x, p1.y)
+      this.ctx.lineTo(p2.x, p2.y)
+      this.ctx.lineTo(end.x, end.y)
+      this.ctx.fill()
+      this.ctx.stroke()
     },
 
     // センサーからデータ取得
@@ -165,6 +222,7 @@ export default {
       }
     }
   },
+
   async mounted() {
     this.canvas = this.$refs.canvas
     this.ctx = this.canvas.getContext('2d')
@@ -181,7 +239,7 @@ export default {
     }
 
     // 矢印の描画
-    this.drawArrow()
+    this.drawAllArrows()
   },
 }
 </script>
