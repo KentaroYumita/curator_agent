71c46b8663aa4e687c35e01992b8cdcb87e003de
Makoto-Takeuchi <t419mako@gmail.com>
2021-10-02 00:30:45 +0900

    Canvas

--- src/main/webapp/src/views/peopleMap.vue
+++ src/main/webapp/src/views/peopleMap.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="example">
     <p>↓</p>
-    <MyCanvas :radius="radius"/>
-    <my-canvas :radius="radius"/>
+    <canvas width="200" height="200" class="canvas"></canvas>
+<!--    <MyCanvas :radius="radius"/>-->
     <p>↑</p>
     <p><input type="range" min="0" max="100" v-model.number="radius"></p>
   </div>
@@ -11,17 +11,37 @@
 <script>
 import MyCanvas from '../components/MyCanvas.vue'
 export default {
-  components: {
-    MyCanvas
+  mounted() {
+    this.ctx = this.canvas.getContext('2d')
+    this.draw(this.radius)
   },
-  data() {
-    return {
-      radius: 50
+
+  props: {
+    radius: {
+      type: Number,
+      default: 50
     }
-  }
+  },
+  watch: {
+    radius() {
+      this.draw(this.radius)
+    }
+  },
+  methods: {
+    draw(radius) {
+      this.ctx.beginPath()
+      this.ctx.clearRect(0, 0, 200, 200)
+      this.ctx.arc(100, 100, radius, 0, Math.PI * 2)
+      this.ctx.fill()
+    }
+  },
+
+
 }
 </script>
 
 <style scoped>
-
+.canvas {
+  border: 1px solid #000;
+}
 </style>
